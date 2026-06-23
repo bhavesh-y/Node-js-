b2dd2e70d75841838ffedc32a260befaf2ec4b73
Bhavesh Yadav <bhaveshyadav@Bhaveshs-MacBook-Air.local>
2026-06-23 10:21:32 +0530

    fs module

---
+++ Fs module/file.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+
+// Sync..
+// fs.writeFileSync('./test.txt','Hey Where');
+
+
+// Async..
+// fs.writeFile('./test.txt','Hey Phere', (err)=>{});
+
+
+
+
+
+
+
+// Sync..
+// const result = fs.readFileSync('./contact.txt',"utf-8")
+// console.log(result)
+
+
+
+// Async..
+// fs.readFile('./contact.txt','utf-8',(err,result)=>{
+//     if (err){
+//         console.log("Error : ",err)
+//     } else{
+//         console.log(result)
+//     }
+// })
+
+
+
+// fs.appendFileSync('./test.txt',new Date().getDate().toLocaleString());
+// fs.appendFileSync('./test.txt',"hey There \n");
+// fs.copyFileSync('./contact.txt','./copy.txt')
+
+
+// fs.unlinkSync("./copy.txt")
+
+console.log(fs.statSync('./test.txt'));
+
+
+
+fs.mkdirSync('my-docs/a/b',{recursive : true})
